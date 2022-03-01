3540f6c8e5a14c0e4a7633acf983e4a077327fba
sushmareddykoncha <100554646+sushmareddykoncha@users.noreply.github.com>
2022-03-01 11:42:49 +0530

    Program.cs
    1.Find the greatest of 3 numbers.
    2.Swap 2 numbers without using 3rd variable.
    3. Get emp details and display them.

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        
+        {   
+            //conditional statements//
+            int a,b,c;
+            Console.WriteLine("Enter the values");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            if(a>b)
+            {
+                if(a>c)
+                {
+                    Console.WriteLine("a is greater");
+                }
+                else
+                {
+                    Console.WriteLine("c is greater");
+                }
+            }
+            else if(b>c)
+            {
+                Console.WriteLine("b is greater");
+            }
+            else
+            {
+                Console.WriteLine("c is greater");
+            }
+            //swap 2 numbers 
+            int num1, num2;
+            Console.WriteLine("Enter two numbers");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+            Console.WriteLine("num1=" + num1);
+            Console.WriteLine("num2=" + num2);
+           
+
+            //get emp details and display them//
+            Console.WriteLine("Enter the employee details");
+            String empid,name;
+            int age;
+            float salary;
+            empid = Console.ReadLine();
+            name = Console.ReadLine();
+            age = int.Parse(Console.ReadLine());
+            salary = float.Parse(Console.ReadLine());
+            Console.WriteLine("empid=" + empid);
+            Console.WriteLine("name=" + name);
+            Console.WriteLine("age=" + age);
+            Console.WriteLine("salary=" + salary);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
